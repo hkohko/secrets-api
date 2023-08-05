fd8583f6d9176fb725e70850ccc6d679596e896d
heko <91366057+hkohko@users.noreply.github.com>
2023-08-05 16:53:22 +0700

    updated password generator

--- app/internals/token.py
+++ app/internals/token.py
@@ -1,12 +1,12 @@
-from secrets import SystemRandom, token_urlsafe
-
-
-def create_instance():
-    return SystemRandom()
+from string import ascii_letters, digits, punctuation
+from secrets import SystemRandom, choice, token_hex
 
 
 def generate_token(entry: int):
     if entry < 32:
         return "length must be >32"
-    generate_nbyte = create_instance().randint(32, entry)
-    return token_urlsafe(generate_nbyte)
+    generate_nbyte = SystemRandom().randint(32, entry)
+    token = token_hex(generate_nbyte)
+    compilation = token + " " + ''.join(choice(ascii_letters) for _ in range(entry)) + digits + punctuation
+    password = ''.join(choice(compilation) for _ in range(entry))
+    return password
